3976bb34f008cbf27dabaf67655dad59d7d050a3
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-04-24 22:17:49 +0200

    feat: Add task lists
    - Add a list of basic tasks

--- learning-platform/src/App.tsx
+++ learning-platform/src/App.tsx
@@ -1,21 +1,19 @@
 import Playground from "./components/Playground/Playground";
-import TaskPanel, { Task } from "./components/TaskPanel/TaskPanel";
+import TaskPanel from "./components/TaskPanel/TaskPanel";
 import "./App.css";
+import { tasks } from "./data/tasks";
 
 function App() {
-  const sampleTask: Task = {
-    title: "Zadanie 1",
-    description: `Napisz program, który wczytuje dwie liczby i wypisuje ich sumę (+).`,
-  };
-
   return (
     <div className="flex h-screen bg-gradient-to-b from-indigo-400">
       <div className="flex-1">
         <Playground />
       </div>
 
-      <div className="w-1/3 p-4 overflow-auto">
-        <TaskPanel task={sampleTask} />
+      <div className="w-1/3 p-4 overflow-auto space-y-2">
+        {tasks.map((task, index) => (
+          <TaskPanel key={index} task={task} />
+        ))}
       </div>
     </div>
   );
---
+++ learning-platform/src/data/tasks.ts
@@ -0,0 +1,40 @@
+import { Task } from "../components/TaskPanel/TaskPanel";
+
+export const tasks: Task[] = [
+  {
+    title: "Zadanie 1",
+    description: `Napisz program, który wczytuje dwie liczby i wypisuje ich sumę (+).`,
+  },
+  {
+    title: "Zadanie 2",
+    description: `Napisz program, który wczytuje dwie liczby i wypisuje ich iloczyn (*).`,
+  },
+  {
+    title: "Zadanie 3",
+    description: `Napisz program, który wczytuje dwie liczby, i wypisuje, czy pierwsza z nich jest mniejsza od drugiej.`,
+  },
+  {
+    title: "Zadanie 4",
+    description: `Napisz program, który wczytuje wiek i sprawdza, czy jesteś pełnoletni.`,
+  },
+  {
+    title: "Zadanie 5",
+    description: `Napisz program, który wczytuje napis i liczbę (n), po czym n-razy wypisuje ten wczytany napis.`,
+  },
+  {
+    title: "Zadanie 6",
+    description: `Napisz program, który wczytuje Twoje imię i wypisuje napis “Hello” + imię.`,
+  },
+  {
+    title: "Zadanie 7",
+    description: `Napisz program, który policzy sumę liczb od 1 do 100.`,
+  },
+  {
+    title: "Zadanie 8",
+    description: `Napisz program, który wczyta liczbę (a) i wypisze sumę 10 kolejnych liczb od a.`,
+  },
+  {
+    title: "Zadanie 9",
+    description: `Napisz program, który wczyta dwie liczby (a i b) i wypisze wszystkie liczby całkowite od a do b.`,
+  },
+];
