855887e1a4dd2a8bbb948ee9b334f89d65f992ff
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-04-24 22:40:44 +0200

    feat: Add checking finished tasks functionality
    - Clicking on the task panel marks the task as `Done`.
    - Clicking again unmarks it

--- learning-platform/src/components/TaskPanel/TaskPanel.tsx
+++ learning-platform/src/components/TaskPanel/TaskPanel.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 export interface Task {
   title: string;
   description: string;
+  completed: boolean;
 }
 
 interface TaskPanelProps {
@@ -8,9 +11,25 @@ interface TaskPanelProps {
 }
 
 const TaskPanel = ({ task }: TaskPanelProps) => {
+  const [isDone, setIsDone] = useState(task.completed);
+  const bgClass = isDone ? "bg-green-100" : "bg-slate-50";
+
   return (
-    <div className="p-4 bg-slate-50 shadow rounded-lg w-full">
-      <h2 className="text-xl font-bold mb-2">{task.title}</h2>
+    <div
+      onClick={() => setIsDone((d) => !d)}
+      className={`p-4 ${bgClass} shadow rounded-lg w-full cursor-pointer
+                transition duration-300 ease-in-out hover:-translate-y-0.5`}
+    >
+      <div className="flex items-center mb-2">
+        <h2 className="text-xl font-bold flex items-center">
+          {task.title}
+          {isDone && (
+            <span className="ml-2 px-2 py-0.5 text-sm font-medium text-green-800 bg-green-200 rounded">
+              Zrobione!
+            </span>
+          )}
+        </h2>
+      </div>
       <p className="text-gray-700">{task.description}</p>
     </div>
   );
--- learning-platform/src/data/tasks.ts
+++ learning-platform/src/data/tasks.ts
@@ -4,37 +4,46 @@ export const tasks: Task[] = [
   {
     title: "Zadanie 1",
     description: `Napisz program, który wczytuje dwie liczby i wypisuje ich sumę (+).`,
+    completed: false,
   },
   {
     title: "Zadanie 2",
     description: `Napisz program, który wczytuje dwie liczby i wypisuje ich iloczyn (*).`,
+    completed: false,
   },
   {
     title: "Zadanie 3",
     description: `Napisz program, który wczytuje dwie liczby, i wypisuje, czy pierwsza z nich jest mniejsza od drugiej.`,
+    completed: false,
   },
   {
     title: "Zadanie 4",
     description: `Napisz program, który wczytuje wiek i sprawdza, czy jesteś pełnoletni.`,
+    completed: false,
   },
   {
     title: "Zadanie 5",
     description: `Napisz program, który wczytuje napis i liczbę (n), po czym n-razy wypisuje ten wczytany napis.`,
+    completed: false,
   },
   {
     title: "Zadanie 6",
     description: `Napisz program, który wczytuje Twoje imię i wypisuje napis “Hello” + imię.`,
+    completed: false,
   },
   {
     title: "Zadanie 7",
     description: `Napisz program, który policzy sumę liczb od 1 do 100.`,
+    completed: false,
   },
   {
     title: "Zadanie 8",
     description: `Napisz program, który wczyta liczbę (a) i wypisze sumę 10 kolejnych liczb od a.`,
+    completed: false,
   },
   {
     title: "Zadanie 9",
     description: `Napisz program, który wczyta dwie liczby (a i b) i wypisze wszystkie liczby całkowite od a do b.`,
+    completed: false,
   },
 ];
