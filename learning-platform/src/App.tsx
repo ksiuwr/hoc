4147ee39631b210fa6a3cd74873c27921f10337e
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-05-20 15:57:03 +0200

    fix: Adjust URL

--- learning-platform/src/App.tsx
+++ learning-platform/src/App.tsx
@@ -1,24 +1,10 @@
 import "./App.css";
-import {
-  Navigate,
-  Route,
-  BrowserRouter as Router,
-  Routes,
-} from "react-router-dom";
 import { tasks } from "./data/tasks";
 import Tasks from "./views/Tasks/Tasks";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate to="/zadania" replace />}
-        />
-        <Route path="/zadania" element={<Tasks tasks={tasks} />} />
-      </Routes>
-    </Router>
+    <Tasks tasks={tasks} />
   );
 }
 
