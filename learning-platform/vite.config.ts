8e0ef0786c36894c68c1457d7d8c52628733ba21
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-05-20 16:42:01 +0200

    fix: Update URLs to match new repository name

--- learning-platform/vite.config.ts
+++ learning-platform/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Online-Python-Learning-Platform',
+  base: '/Python-Learning-Platform',
 })
